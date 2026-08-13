0cc2461a47591903a19e606e2b902cd9f8b79b3d
이백행 <leebh@itcen.com>
2026-08-13 12:54:06 +0900

    feat(mapper): SWAT MyBatis 매퍼 스캔 및 XML 리소스 등록
    - MapperScan 대상 패키지에 swat 추가
    - 기존 전자정부 매퍼와 SWAT 매퍼 XML을 통합하여 등록
    - 하위 경로의 SWAT 매퍼 XML을 탐색하도록 패턴 적용

--- swat.ibatch-web/src/main/java/egovframework/example/bat/config/EgovConfigMapper.java
+++ swat.ibatch-web/src/main/java/egovframework/example/bat/config/EgovConfigMapper.java
@@ -1,18 +1,23 @@
 package egovframework.example.bat.config;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
+@MapperScan(basePackages = { "egovframework", "swat" })
 public class EgovConfigMapper {
 
 	@Bean(name = "egov.sqlSession")
@@ -22,8 +27,20 @@ public class EgovConfigMapper {
 		sqlSessionFactoryBean.setDataSource(dataSource);
 		sqlSessionFactoryBean
 				.setConfigLocation(pmrpr.getResource("classpath:/egovframework/mapper/config/mapper-config.xml"));
-		sqlSessionFactoryBean.setMapperLocations(
-				pmrpr.getResources("classpath:/egovframework/mapper/example/bat/Egov_Example_SQL.xml"));
+//		sqlSessionFactoryBean.setMapperLocations(
+//				pmrpr.getResource("classpath:/egovframework/mapper/example/bat/Egov_Example_SQL.xml"),
+//				pmrpr.getResource("classpath:/swat/mapper/example/**/.*.xml"));
+
+		Resource[] egovResources = pmrpr
+				.getResources("classpath:/egovframework/mapper/example/bat/Egov_Example_SQL.xml");
+
+		Resource[] swatResources = pmrpr.getResources("classpath*:/swat/mapper/example/**/*.xml");
+
+		Resource[] mapperLocations = Stream.concat(Arrays.stream(egovResources), Arrays.stream(swatResources))
+				.toArray(Resource[]::new);
+
+		sqlSessionFactoryBean.setMapperLocations(mapperLocations);
+
 		/*
 		 * classpath:/egovframework/mapper/example/bat/Egov_Example_SQL_altibase.xml
 		 * classpath:/egovframework/mapper/example/bat/Egov_Example_SQL_cubrid.xml
