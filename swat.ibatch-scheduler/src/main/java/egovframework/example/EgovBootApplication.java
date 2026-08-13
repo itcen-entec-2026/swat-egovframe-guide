80efe8ad08166fcbf4a4d927f3027e1fb03e5a5f
이백행 <leebh@itcen.com>
2026-08-13 09:39:07 +0900

    Source Format

--- swat.ibatch-scheduler/src/main/java/egovframework/example/EgovBootApplication.java
+++ swat.ibatch-scheduler/src/main/java/egovframework/example/EgovBootApplication.java
@@ -16,22 +16,23 @@ import org.springframework.boot.WebApplicationType;
  * @since 2021. 11.25
  * @version 1.0
  * @see
- *  <pre>
+ * 
+ *      <pre>
  *      개정이력(Modification Information)
  *
  *  수정일               수정자                 수정내용
  *  ----------   -----------   ---------------------------
  *  2021.11.25   신용호                 최초 생성
- *  
- *  </pre>
+ * 
+ *      </pre>
  */
 
 public class EgovBootApplication implements CommandLineRunner {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(EgovBootApplication.class);
 
 	public static void main(String[] args) {
-		//SpringApplication.run(EgovBootApplication.class, args);
+		// SpringApplication.run(EgovBootApplication.class, args);
 
 		System.out.println("##### EgovSampleBootApplication Start #####");
 
@@ -40,13 +41,13 @@ public class EgovBootApplication implements CommandLineRunner {
 		springApplication.setHeadless(false);
 		springApplication.setBannerMode(Banner.Mode.CONSOLE);
 		springApplication.run(args);
-		
+
 		System.out.println("##### EgovSampleBootApplication End #####");
 	}
-	
+
 	@Override
-    public void run(String... args) throws Exception {
-		
+	public void run(String... args) throws Exception {
+
 		List<String> jobPaths = new ArrayList<String>();
 
 		/*
@@ -63,16 +64,16 @@ public class EgovBootApplication implements CommandLineRunner {
 
 		/*
 		 * EgovSchedulerRunner에 contextPath, schedulerJobPath, jobPaths를 인수로 넘겨서 실행한다.
-		 * contextPath: Batch Job 실행에 필요한 context 정보가 기술된 xml파일 경로
-		 * schedulerJobPath: Scheduler의 Trigger가 수행할 SchedulerJob(ex: QuartzJob)이 기술된 xml파일 경로
-		 * jobPaths: Batch Job이 기술 된 xml 파일 경로들
-		 * delayTime: Scheduler 실행을 위해 ApplicationContext를 종료를 지연시키는 시간(실행시간)
-		 *            (기본 30000 milliseconds: 30초)
+		 * contextPath: Batch Job 실행에 필요한 context 정보가 기술된 xml파일 경로 schedulerJobPath:
+		 * Scheduler의 Trigger가 수행할 SchedulerJob(ex: QuartzJob)이 기술된 xml파일 경로 jobPaths:
+		 * Batch Job이 기술 된 xml 파일 경로들 delayTime: Scheduler 실행을 위해 ApplicationContext를
+		 * 종료를 지연시키는 시간(실행시간) (기본 30000 milliseconds: 30초)
 		 */
-		EgovSchedulerRunner egovSchedulerRunner = new EgovSchedulerRunner("/egovframework/batch/context-batch-scheduler.xml", "/egovframework/batch/context-scheduler-job.xml",
+		EgovSchedulerRunner egovSchedulerRunner = new EgovSchedulerRunner(
+				"/egovframework/batch/context-batch-scheduler.xml", "/egovframework/batch/context-scheduler-job.xml",
 				jobPaths, 30000);
 		egovSchedulerRunner.start();
-		
-    }
+
+	}
 
 }
