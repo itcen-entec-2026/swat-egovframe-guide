711c643bde94cd7171dcb9cbb019060bf9f60c7d
이백행 <leebh@itcen.com>
2026-08-13 10:17:08 +0900

    Source Format

--- swat.ibatch-web/src/main/java/egovframework/example/EgovBootApplication.java
+++ swat.ibatch-web/src/main/java/egovframework/example/EgovBootApplication.java
@@ -12,15 +12,16 @@ import org.springframework.context.annotation.Import;
  * @since 2021. 11.25
  * @version 1.0
  * @see
- *  <pre>
+ * 
+ *      <pre>
  *      개정이력(Modification Information)
  *
  *  수정일         수정자          수정내용
  *  ----------   -----------   ---------------------------
  *  2021.11.25   신용호          최초 생성
  *  2023.03.15   신용호          java Config 및 Annotation 적용
- *  
- *  </pre>
+ * 
+ *      </pre>
  */
 
 @SpringBootApplication(scanBasePackages = "egovframework")
@@ -29,21 +30,19 @@ import org.springframework.context.annotation.Import;
 public class EgovBootApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(EgovBootApplication.class, args);
+		// SpringApplication.run(EgovBootApplication.class, args);
 
 		System.out.println("##### EgovSampleBootApplication Start #####");
 
-		ApplicationContext context = new SpringApplicationBuilder(EgovBootApplication.class)
-                .headless(false)
-                .bannerMode(Banner.Mode.CONSOLE)
-                .run(args);
+		ApplicationContext context = new SpringApplicationBuilder(EgovBootApplication.class).headless(false)
+				.bannerMode(Banner.Mode.CONSOLE).run(args);
 		/*
-		SpringApplication springApplication = new SpringApplication(EgovBootApplication.class);
-		springApplication.setBannerMode(Banner.Mode.OFF);
-		springApplication.setLogStartupInfo(false);
-		springApplication.run(args);
-		*/
+		 * SpringApplication springApplication = new
+		 * SpringApplication(EgovBootApplication.class);
+		 * springApplication.setBannerMode(Banner.Mode.OFF);
+		 * springApplication.setLogStartupInfo(false); springApplication.run(args);
+		 */
 		System.out.println("##### EgovSampleBootApplication End #####");
 	}
-	
+
 }
